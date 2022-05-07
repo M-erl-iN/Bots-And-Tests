a5b963a2902eb5a2b5dba7632077a62bd5f4ffbd
M_erl_iN <erlan10122005@gmail.com>
2022-05-07 12:11:33 +0300

    Наконец-то, я собрал их ВСЕ!

--- 49. Библиотеки unittest и pytest/Дополнительные задачи/Тестирование с помощью unittest.py
+++
@@ -1,9 +0,0 @@
-from yandex_testing_lesson import (TestReverse, TypeError, 1, "__main__":, '1',
-                                   '3', '7'], '', 'abccba', 'abcd', 'dcba', :,
-                                   ==, ['1', __name__, def, if, reverse,
-                                   reverseimport, self, self.assertEqual,
-                                   self.assertRaises, test_empty,
-                                   test_iterable_object, test_normal_data,
-                                   test_one_symbol, test_pallindrome,
-                                   test_uniterable_object, unittest.main,
-                                   unittest.TestCase, unittestclass, with)
